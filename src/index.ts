c210dd054ec5057178b6efadb60091b4f54f9681
Fabio Matsumoto <hide.fabio123@gmail.com>
2022-07-17 22:33:03 -0300

    feat: increment entity post

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 
 const main = async () => {
   const orm = await MikroORM.init({
-    entities: [],
+    entities: [Post],
+    user: "postgres",
+    password: "postgres",
     dbName: "mikro-orm",
     type: "postgresql",
     debug: !__prod__,
+    allowGlobalContext: true,
   });
+  //@ts-ignore
+  const post = orm.em.create(Post, {title: 'my first post'});
+  await orm.em.persistAndFlush(post);
+  console.log("----> sql 2");
+  await orm.em.nativeInsert(Post, {title: 'my first post 2'});
 };
 
 
-console.log("Hello world!");
+main().catch((err) => {
+    console.log(err);
+});

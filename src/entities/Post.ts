d6fcfbf2a459d1ff4247db466a9ee682a7c0d0f6
Fabio Matsumoto <hide.fabio123@gmail.com>
2022-07-21 23:01:26 -0300

    shore: adjust in post

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -5,8 +5,8 @@ export class Post {
 	@PrimaryKey()
 	id!: number;
 
-  @Property()
-  createdAt = new Date();
+	@Property()
+	createdAt = new Date();
 
 	@Property({ onUpdate: () => new Date() })
 	updatedAt = new Date();

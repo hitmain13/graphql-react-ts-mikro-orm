6fceb6f55338de287748fc357f3e2b0d422a7283
Fabio Matsumoto <hide.fabio123@gmail.com>
2022-07-25 17:49:32 -0300

    create migration

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+import microConfig from './mikro-orm.config';
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    user: "postgres",
-    password: "postgres",
-    dbName: "mikro-orm",
-    type: "postgresql",
-    debug: !__prod__,
-    allowGlobalContext: true,
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
   });
-  //@ts-ignore
-  const post = orm.em.create(Post, {title: 'my first post'});
-  await orm.em.persistAndFlush(post);
-  console.log("----> sql 2");
-  await orm.em.nativeInsert(Post, {title: 'my first post 2'});
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => console.log("server started on localhost:4000"))
 };
 
 
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,16 +1,18 @@
-import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
-import path from "path";
+import path from 'path';
 
-console.log("dirname", __dirname);
 export default {
   migrations: {
-    path: path.join(__dirname, "./migrations"),
-    patterns: /^[\w-]+\d+\.[tj]s$/,
+    path: path.join(__dirname, "./migrations"), 
+    pattern: "!(*.d).{js,ts}", 
   },
   entities: [Post],
+  user: "postgres",
+  password: "postgres",
   dbName: "mikro-orm",
   type: "postgresql",
   debug: !__prod__,
+  allowGlobalContext: true,
 } as Parameters<typeof MikroORM.init>[0];
